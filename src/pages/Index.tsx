7f6d4e2b3705c7eb5f09602124ac983324fa9c4e
Anay Tondar <anaytondar55@gmail.com>
2025-11-27 15:33:08 +0530

    updates

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background text-foreground">
       <SupplyChainCalculator />
-      <MadeWithDyad />
+      {/* <MadeWithDyad /> */}
     </div>
   );
 };
